023e144cd129e12bd00c4a68c392be2b095e0758
SufBit <sufarshad.99@gmail.com>
2024-05-08 13:56:15 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
-
 import { AppProvider } from './context/AppContext';
-import Remaining from './components/Remaining';
+import Budget from './components/Budget';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
 import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
 
 const App = () => {
     return (
@@ -25,13 +18,10 @@ const App = () => {
                         <Budget />
                     </div>
                     <div className='col-sm'>
-                        <Remaining />
+                        <RemainingBudget />
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
-                    </div>
-                    <div className='col-sm'>
-                        <ExpenseItem />
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
@@ -50,4 +40,5 @@ const App = () => {
         </AppProvider>
     );
 };
+
 export default App;
